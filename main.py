926c00099de0669039c25f2393b06e8ca45dc80c
segovelo <gauchosinpingo@gmail.com>
2022-10-31 20:25:53 +0000

    Changes to main.py

--- main.py
+++ main.py
@@ -14,24 +14,30 @@ class Assistant:
         self.speaker = tts.init()
         self.speaker.setProperty("rate", 150)
 
-        self.assistant = GenericAssistant("intents.json", intent_methods={
-                                          "file": self.create_file})
+        self.assistant = GenericAssistant("intents.json", intent_methods=self.mappings)
+                                          #"file": self.create_file})
         self.assistant.train_model()
 
         self.root = tk.Tk()
-        self.label = tk.Label(text="🤖", font=("Arial", 120, "bold"))
-        self.stopButton = tk.Button(justify="left", height=2, width=20, text ="Stop", command = exit)
+        self.label = tk.Label(text="🤖", font=("Arial", 150, "bold"))
         self.label.pack()
-        self.stopButton.pack()
 
         threading.Thread(target=self.run_assistant).start()
 
         self.root.mainloop()
 
     def create_file(self):
+        print("Creating a new file...")
+        self.speaker.say("Creating a new file")
+        self.speaker.runAndWait()
         with open("test.txt", "w") as f:
             f.write("This a test text file")
             f.close()
+
+    def hello(self):
+        print("Inside hello function...")
+        self.speaker.say("Hello Sebastian , how are you today?")
+        self.speaker.runAndWait()
 
     def run_assistant(self):
         while True:
@@ -43,7 +49,8 @@ class Assistant:
                     text = self.recognizer.recognize_google(audio)
                     text = text.lower()
 
-                    if "wake up" in text:
+                    if "listen" in text:
+                        print("Listening... 🎤")
                         self.label.config(fg="red")
                         audio = self.recognizer.listen(mic)
                         text = self.recognizer.recognize_google(audio)
@@ -58,11 +65,24 @@ class Assistant:
                             if text is not None:
                                 response = self.assistant.request(text)
                                 if response is not None:
+                                    print("Bot: ",response)
                                     self.speaker.say(response)
                                     self.speaker.runAndWait()
                             self.label.config(fg="black")
+            except speech_recognition.UnknownValueError:
+                print("Exception  speech_recognition.UnknownValueError !")
+                self.recognizer = speech_recognition.Recognizer()
+                self.speaker.say("I did not understand, please try again")
+                self.speaker.runAndWait()
+
             except:
+                print("Exception occurred !")
                 self.label.config(fg="black")
                 continue
+    mappings = {
+        "file": create_file,
+        "greeting": hello
+    }
+
 
 Assistant()
